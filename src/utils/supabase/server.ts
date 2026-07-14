ab9e61ce49c240a6fad71b690b890a4f2f468d64
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-07-15 00:53:40 +0500

    Added supabase files

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,36 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = async () => {
+  const cookieStore = await cookies();
+
+  const supabase = createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // Called from a Server Component — safe to ignore.
+            // Middleware handles session refresh.
+          }
+        },
+      },
+    }
+  );
+
+  // Force session to load from cookies into client's internal state
+  await supabase.auth.getSession();
+
+  return supabase;
+};
